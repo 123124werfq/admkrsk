08703bfa2618c532167b12256c7815555e30e549
msd <ms@lobzenko.ru>
2019-11-25 04:34:57 +0700

    up

---
+++ common/fixtures/data/service_target.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'id_target' => '',
+        'id_service' => null,
+        'id_form' => null,
+        'name' => null,
+        'reestr_number' => null,
+        'modified_at' => null,
+        'created_at' => null,
+        'created_by' => null,
+        'updated_at' => null,
+        'updated_by' => null,
+        'deleted_at' => null,
+        'deleted_by' => null,
+        'target' => null,
+        'target_code' => null,
+        'service_code' => null,
+        'obj_name' => null,
+        'place' => null,
+        'state' => null,
+    ],
+];
