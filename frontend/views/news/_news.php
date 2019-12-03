18e3241998b3fd9f764f51e6c0240066af21667a
msd <ms@lobzenko.ru>
2019-12-04 01:24:23 +0700

    up

--- frontend/views/news/_news.php
+++ frontend/views/news/_news.php
@@ -1,10 +1,12 @@
 <?php
 	use yii\helpers\Html;
 	use yii\bootstrap\ActiveForm;
+
+    $url = $data->getUrl();
 ?>
 <div class="press-item">
     <div class="press-item_content">
-        <h4 class="press_title"><a href="?id=<?=$data->id_news?>"><?=Html::encode($data->title)?></a></h4>
+        <h4 class="press_title"><a href="<?=$url?>"><?=Html::encode($data->title)?></a></h4>
         <ul class="press_info">
             <?php if (!empty($data->id_rub)){?>
             <li class="press_info-item press_info-item__place"><a href="?id_rub=<?=$data->id_rub?>"><?=$data->rub->getLineValue()?></a></li>
@@ -13,7 +15,7 @@
         </ul>
     </div>
     <?php if (!empty($data->id_media)){?>
-    <a href="?id=<?=$data->id_news?>" class="press_img-holder">
+    <a href="<?=$url?>" class="press_img-holder">
         <img class="press_img img-responsive" src="<?=$data->makeThumb(['w'=>768,'h'=>480])?>" alt="<?=Html::encode($data->title)?>">
     </a>
     <?php }?>
