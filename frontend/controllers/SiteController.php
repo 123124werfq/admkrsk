23f7171d1436fa5ba45df919704b34b861b887bb
Konstantin Bogdanov <kosyag@gmail.com>
2019-12-05 01:52:25 +0700

    test

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -533,13 +533,61 @@ class SiteController extends Controller
         }
     }
 
+
+    public function actionWtest()
+    {
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+        $section = $phpWord->addSection();
+
+        $section->addText(
+            '"Learn from yesterday, live for today, hope for tomorrow. '
+            . 'The important thing is not to stop questioning." '
+            . '(Albert Einstein)'
+        );
+
+        $section->addText(
+            '"Great achievement is usually born of great sacrifice, '
+            . 'and is never the result of selfishness." '
+            . '(Napoleon Hill)',
+            array('name' => 'Tahoma', 'size' => 10)
+        );
+
+        $fontStyleName = 'oneUserDefinedStyle';
+        $phpWord->addFontStyle(
+            $fontStyleName,
+            array('name' => 'Tahoma', 'size' => 10, 'color' => '1B2232', 'bold' => true)
+        );
+        $section->addText(
+            '"The greatest accomplishment is not in never falling, '
+            . 'but in rising again after you fall." '
+            . '(Vince Lombardi)',
+            $fontStyleName
+        );
+
+        $html = "<h1>ВСЕМ ПРИВЕТ!</h1><p>Это абзац</p><table><tr><td>А</td><td>Это</td><td>Таблица</td></tr></table>";
+
+        //$section2 = $phpWord->addSection();
+        \PhpOffice\PhpWord\Shared\Html::addHtml($section, $html);
+
+        $fontStyle = new \PhpOffice\PhpWord\Style\Font();
+        $fontStyle->setBold(true);
+        $fontStyle->setName('Tahoma');
+        $fontStyle->setSize(13);
+        $myTextElement = $section3->addText('"Believe you can and you\'re halfway there." (Theodor Roosevelt)');
+        $myTextElement->setFontStyle($fontStyle);
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        $objWriter->save('../runtime/helloWorld.docx');
+    }
+
     /**
      * @throws NotFoundHttpException
      */
     public function actionFakelogin()
     {
         if (YII_ENV_DEV) {
-            $user = User::findOne(1);
+            $user = User::findOne(8);
 
             Yii::$app->user->login($user, 3600*24*7);
 
