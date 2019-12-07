70fa4670e489b6a201b92121ed86bede1fa89b30
Lobzenko <ms@lobzenko.ru>
2019-12-07 15:06:26 +0700

    up

--- backend/views/form-input/_element_options.php
+++ backend/views/form-input/_element_options.php
@@ -1,10 +1,25 @@
 <?php
 	use yii\helpers\Html;
+	use common\models\FormInput;
+	use common\models\CollectionColumn;
+	use yii\helpers\ArrayHelper;
 
 	$data = $element->getOptionsData();
 
-	echo '<div class="row-flex">';
+	$visibleInputs = [];
 
+	$inputs = FormInput::find()->where([
+				'id_form'=>$id_form,
+				'type'=>[CollectionColumn::TYPE_SELECT,
+						 CollectionColumn::TYPE_CHECKBOX]
+			  ]);
+
+	if (!empty($input))
+		$inputs->andWhere('id_input <> '.(int)$input->id_input);
+
+    $visibleInputs = ArrayHelper::map($inputs->all(), 'id_input', 'name');
+
+	echo '<div class="row-flex">';
 	foreach ($data as $key => $option)
 	{
 		$option['class'] = 'form-control';
@@ -14,4 +29,30 @@
 		echo '</div>';
 	}
 	echo '</div>';
+
+	if (!empty($visibleInputs)){
+        $records = $element->getRecords('visibleInputs');
+    ?>
+        <p>Отображать если:</p>
+        <div id="visibles" class="multiyiinput">
+        <?php foreach ($records as $key => $visibleInput) {?>
+            <div class="row">
+                <div class="col-sm-5">
+                <?=$form->field($visibleInput, "[visibleInputs][$key]id",['template'=>"{input}"])->hiddenInput()?>
+                    <?=$form->field($visibleInput, "[visibleInputs][$key]id_input_visible",['template'=>"{input}"])->dropDownList($visibleInputs,['class'=>'form-control visible-field','prompt'=>'Выберите поле зависимости'])?>
+                </div>
+                <div class="col-sm-6 visibleInputValues">
+                    <?=(!empty($visibleInput->id_input_visible))?$this->render('_input',['visibleInput'=>$visibleInput->visibleInput,'model'=>$visibleInput,'form'=>$form,'rowKey'=>$key]):''?>
+                </div>
+                <div class="col-sm-1">
+                    <a class="close" href="javascript:">&times;</a>
+                </div>
+            </div>
+        <?php
+            }
+        ?>
+        </div>
+        <a onclick="return addInput('visibles')" href="#" class="btn btn-default btn-visible">Добавить еще</a>
+<?php
+    }
 ?>
--- common/models/FormVisibleinput.php
+++ common/models/FormVisibleinput.php
@@ -29,8 +29,8 @@ class FormVisibleInput extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id_input', 'id_input_visible'], 'default', 'value' => null],
-            [['id_input', 'id_input_visible'], 'integer'],
+            [['id_element', 'id_input_visible'], 'default', 'value' => null],
+            [['id_element', 'id_input_visible'], 'integer'],
             [['values'], 'safe'],
         ];
     }
@@ -42,9 +42,9 @@ class FormVisibleInput extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'id_input' => 'Id Input',
-            'id_input_visible' => 'Id Input Visible',
-            'values' => 'Values',
+            'id_element' => 'Элемент формы',
+            'id_input_visible' => 'Если в поле',
+            'values' => 'Значения',
         ];
     }
 
