263d7b47410a941d84b0d07c48a6222fe09891a8
Konstantin Bogdanov <kosyag@gmail.com>
2019-12-14 17:03:48 +0700

    сохранение анкеты

--- frontend/controllers/ReserveController.php
+++ frontend/controllers/ReserveController.php
@@ -6,17 +6,31 @@ namespace frontend\controllers;
 use Yii;
 use common\models\Page;
 use common\models\Collection;
+use common\models\FormDynamic;
+use yii\web\BadRequestHttpException;
 
 
 class ReserveController extends \yii\web\Controller
 {
-    public function actionCandidateForm($page=nu)
+    public function actionCandidateForm($page=null)
     {
         $inputs = [];
 
         $page = Page::findOne(['alias'=>'candidate-form']);
         $collection = Collection::findOne(['alias'=>'reserv_anketa']);
 
+        if(!$collection || !$page)
+            throw new BadRequestHttpException();
+
+        $model = new FormDynamic($collection->form);
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            $prepare = $model->prepareData(true);
+
+            if ($record = $collection->insertRecord($prepare)) {
+            }
+
+        }
 
         return $this->render('form', [
             'form'=>$collection->form,
