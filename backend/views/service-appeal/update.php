41023ccb3c0dab256550188bfd6681b7842a423e
Lobzenko <ms@lobzenko.ru>
2019-12-17 13:29:24 +0700

    up

--- backend/views/service-appeal/update.php
+++ backend/views/service-appeal/update.php
@@ -5,14 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\ServiceAppeal */
 
-$this->title = 'Update Service Appeal: ' . $model->id_appeal;
-$this->params['breadcrumbs'][] = ['label' => 'Service Appeals', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id_appeal, 'url' => ['view', 'id' => $model->id_appeal]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Редактирвоать связь: ' . $model->id_appeal;
+$this->params['breadcrumbs'][] = ['label' => 'Связи', 'url' => ['index']];
 ?>
 <div class="service-appeal-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
