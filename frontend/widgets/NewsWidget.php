4325627f1a2bc24a73ea25cf0327b2de14a2c5c8
Lobzenko <ms@lobzenko.ru>
2019-12-25 11:56:24 +0700

    1

--- frontend/widgets/NewsWidget.php
+++ frontend/widgets/NewsWidget.php
@@ -31,6 +31,7 @@ class NewsWidget extends \yii\base\Widget
             if (!empty($wide))
                 $news = News::find()
                         ->where(['state'=>1,'id_page'=>$page->id_page])
+                        ->orWhere("id_news IN (SELECT id_news FROM dbl_news_page WHERE id_page = $page->id_page)")
                         ->andWhere('id_news <> '.$wide->id_news)
                         ->orderBy('date_publish DESC')
                         ->limit(6)
