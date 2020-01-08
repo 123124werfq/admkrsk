44bc3b441d7c3aceed5e3a027412892b3cd1e5bb
Konstantin Bogdanov <kosyag@gmail.com>
2020-01-08 15:37:35 +0700

    upd

--- frontend/views/pravo/detail.php
+++ frontend/views/pravo/detail.php
@@ -49,21 +49,7 @@
 
         <hr class="hr hr__md"/>
 
-        <div class="row">
-            <div class="col-2-third">
-                <p class="text-help">
-                    Дата публикации (изменения): <?=date('d.m.Y',$data->created_at)?> (<?=date('d.m.Y',$data->updated_at)?>)<br>
-                    Просмотров за год (всего): <?=$data->viewsYear?> (<?=$data->views?>)
-                </p>
-                <div class="subscribe">
-                    <div class="subscribe_left">
-                        Поделиться:
-                        <div class="ya-share2 subscribe_share" data-services="vkontakte,facebook,odnoklassniki"></div>
-                    </div>
-                    <div class="subscribe_right"><a class="btn-link" onclick="print()"><i class="material-icons subscribe_print">print</i> Распечатать</a></div>
-                </div>
-            </div>
-        </div>
+        <?= $this->render('//site/_pagestat', ['data' => $data])?>
     </div>
 </div>
 
