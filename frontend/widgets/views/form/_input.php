0c40fab076bd3433b1f66de2c6c4de4d7c3f7ed2
Lobzenko <ms@lobzenko.ru>
2020-01-16 17:51:49 +0700

    up

--- frontend/widgets/views/form/_input.php
+++ frontend/widgets/views/form/_input.php
@@ -101,16 +101,14 @@ $id_subform = (!empty($subform)) ? $subform->id_form : '';
                 echo $form->field($model, $attribute)->textArea($options);
                 break;
             case CollectionColumn::TYPE_REPEAT:
-                /*echo '<div class="checkbox-group">
+                echo '<div class="checkbox-group">
                             <label class="checkbox checkbox__ib">
                                 ' . Html::checkBox('FormDynamic[' . $attribute . '][active]', (!empty($model->$clearAttribute)), $options) . '
                                 <span class="checkbox_label">' . ($input->label ?? $input->name) . '</span>
                             </label>
                         </div>';
-
-                echo $form->field($model, $attribute.'')->dropDownList($input->getArrayValues(), $options);
-
-                echo $form->field($model, $attribute)->textArea($options);*/
+                echo $form->field($model, $attribute.'[repeat]')->dropDownList($input->getArrayValues(), $options);
+                echo $form->field($model, $attribute.'[days]')->textinput($options);
                 break;
             case CollectionColumn::TYPE_ADDRESSES:
                 echo $form->field($model, $attribute)->widget(Select2::class, [
