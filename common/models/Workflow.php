a51a13759355eb7bff6fdfcd651bd3798ecee456
Konstantin Bogdanov <kosyag@gmail.com>
2020-01-18 15:26:13 +0700

    upd

--- common/models/Workflow.php
+++ common/models/Workflow.php
@@ -283,11 +283,11 @@ class Workflow extends Model
             echo "$errstr ($errno)<br />\n";
         } else {
             $out = "POST /WSSiteRSA HTTP/1.1\r\n";
-            $out .= "Host: 10.24.0.201\r\n";
+//            $out .= "Host: 10.24.0.201\r\n";
             $out .= "Content-Length: ".strlen($body)."\r\n";
             $out .= "Content-Type: application/x-www-form-urlencoded\r\n\r\n";
             $out .= $body;
-            $out .= "Connection: Close\r\n\r\n";
+            //$out .= "Connection: Close\r\n\r\n";
             fwrite($fp, $out);
             while (!feof($fp)) {
                 echo fgets($fp, 128);
