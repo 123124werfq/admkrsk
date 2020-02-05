0b126594ef8b1324740eb34eba80886792a9d2df
Lobzenko <ms@lobzenko.ru>
2020-02-05 11:22:30 +0700

    up

--- backend/views/collection/_form_view.php
+++ backend/views/collection/_form_view.php
@@ -84,19 +84,11 @@ if (empty($filtes))
     </div>
 </div>
 <div id="view-columns" class="multiyiinput sortable">
-    <?php
-        //$records = $model->getRecords('columns');
-    ?>
     <?php foreach ($model->getViewColumns() as $key => $data) {?>
         <div class="row">
             <div class="col-md-2">
                 <div class="form-group">
                     <?=Html::dropDownList("ViewColumns[$key][id_column]",$data['id_column'],$columns,['class'=>'form-control','id'=>'CollectionColumn_id_column_'.$key,'placeholder'=>'Выберите колонку']);?>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="form-group">
-                    <?=Html::textInput("ViewColumns[$key][value]",$data['value'],['class'=>'form-control','id'=>'value_'.$key,'placeholder'=>'Введите шаблон']);?>
                 </div>
             </div>
             <div class="col-md-1 col-close">
