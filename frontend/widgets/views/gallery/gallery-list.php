ef26fff8f47cfb7e9904deda6c002811c582f2a7
Lobzenko <ms@lobzenko.ru>
2020-03-26 12:15:19 +0700

    up

--- frontend/widgets/views/gallery/gallery-list.php
+++ frontend/widgets/views/gallery/gallery-list.php
@@ -13,9 +13,10 @@ if (empty($limit)) {
     <?php foreach ($galleries as $gallery): ?>
         <?php
         /** @var Media $firstMedia */
-        $firstMedia = $gallery->medias[0];
-        unset($gallery->medias[0]);
-        $limit = count($gallery->medias) <= $limit ? count($gallery->medias) : $limit;
+        $medias = $gallery->medias;
+        $firstMedia = array_shift($medias);
+
+        $limit = count($medias) <= $limit ? count($medias) : $limit;
         ?>
         <div class="content-gallery_item content-gallery_item__single">
             <a href="<?= $firstMedia->getUrl() ?>" data-fancybox="gallery-<?= $gallery->id_gallery ?>">
@@ -27,7 +28,7 @@ if (empty($limit)) {
                 <p class="content-gallery_item-title"><?= $gallery->name ?></p>
             </a>
             <div class="hidden">
-                <?php foreach ($gallery->medias as $key => $media): ?>
+                <?php foreach ($medias as $key => $media): ?>
                     <?php if ($key >= $limit) break; ?>
                     <a href="<?= $media->getUrl() ?>" data-fancybox="gallery-<?= $gallery->id_gallery ?>">
                         <img src="<?= $media->showThumb(['w' => 768, 'h' => 450]) ?>" alt="">
