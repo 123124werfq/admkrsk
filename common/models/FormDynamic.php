82bf3ff2f57a184719efddb436e3f57cf57cd7c0
msd <ms@lobzenko.ru>
2020-06-22 10:22:20 +0700

    p

--- common/models/FormDynamic.php
+++ common/models/FormDynamic.php
@@ -15,6 +15,7 @@ class FormDynamic extends DynamicModel
     public $form;
     public $data;
     public $postData;
+    public $group;
     protected $labels;
 
     //private $_properties;
@@ -73,6 +74,9 @@ class FormDynamic extends DynamicModel
                     break;
             }
         }
+
+        // для подформ
+        $this->addRule(['group'], 'safe');
     }
 
     public function loadDataFromRecord($data)
