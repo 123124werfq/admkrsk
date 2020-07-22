f2e50c7b89ffd6f89f3397f05e5b86c9dd97fa85
Lobzenko <ms@lobzenko.ru>
2020-07-22 12:57:26 +0700

    up

--- frontend/controllers/UserfirmController.php
+++ frontend/controllers/UserfirmController.php
@@ -67,9 +67,8 @@ class UserfirmController extends \yii\web\Controller
         {
             if (!empty($collection))
             {
-
                 $record = $collection->getDataQuery()
-                    ->whereByAlias($firm=='uk'?['ogrn'=>$model->ogrn]:['inn'=>(int)$model->inn])
+                    ->whereByAlias($type=='uk'?['ogrn'=>$model->ogrn]:['inn'=>(int)$model->inn])
                     ->whereByAlias(['name'=>$model->name])
                     ->limit(1)->getArray();
 
