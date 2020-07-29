dd6f69cfd837ca12e684d50272dd1f0ae4e96572
Lobzenko <ms@lobzenko.ru>
2020-07-29 15:40:24 +0700

    up

--- backend/web/js/admin.js
+++ backend/web/js/admin.js
@@ -122,6 +122,66 @@ tinymce.init(tinymceConfig);
   });
 });*/
 
+function visibleForm(visibleInputs,visibleElements,dom)
+{
+    var $dom = $(dom);
+
+    function getValue(id_input)
+    {
+        var input = $dom.find("#formdynamic-input"+id_input+", .formdynamic-input"+id_input+" input[name*='input"+id_input+"']:checked");
+
+        if (input.is(':checkbox'))
+        {
+            if (input.is(':checked'))
+                var val = input.val();
+            else
+                var val = null
+        }
+        else
+            var val = input.val();
+
+        return val;
+    }
+
+    function check(id_element)
+    {
+        var show = true;
+
+        for (var id_input in visibleElements[id_element])
+        {
+            var value = getValue(id_input);
+
+            if (visibleElements[id_element][id_input].indexOf(value)<0)
+            {
+                show = false;
+                break;
+            }
+        }
+
+        if (show)
+            $dom.find("#element"+id_element).show().find('input, textarea, select').prop('disabled',false);
+        else
+            $dom.find("#element"+id_element).hide().find('input, textarea, select').prop('disabled',true);
+    }
+
+    for (var id_vinput in visibleInputs)
+    {
+        $dom.find("#formdynamic-input"+id_vinput+", .formdynamic-input"+id_vinput+" input[name*='input"+id_vinput+"']").change(function(){
+
+            if ($(this).data('id'))
+                var id = $(this).data('id');
+            else
+                var id = $(this).attr('id').replace('formdynamic-input','');
+
+            for (var id_element in visibleInputs[id])
+                check(id_element);
+        });
+    }
+
+    for (var id_element in visibleElements)
+        check(id_element);
+}
+
 function formTemplateSortable()
 {
   $("#form-template_pjax ").sortable({
