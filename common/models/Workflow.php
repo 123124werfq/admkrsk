c0f96585f1418515ef7a55ad43e0435ae5d672e0
Konstantin Bogdanov <kosyag@gmail.com>
2020-08-11 12:22:33 +0700

    автоматическое формирование XOP

--- common/models/Workflow.php
+++ common/models/Workflow.php
@@ -7,6 +7,9 @@ namespace common\models;
 use Yii;
 use yii\base\Model;
 
+use Selective\XmlDSig\DigestAlgorithmType;
+use Selective\XmlDSig\XmlSigner;
+use XmlDsig\XmlDigitalSignature;
 
 class Workflow extends Model
 {
@@ -546,11 +549,104 @@ class Workflow extends Model
         return $processed;
     }
 
+
+    // метод подписи XML, протестирован руками
+    protected function signServiceXML($sourcePath, $resultPath, $attachment = null)
+    {
+        $certName = "/var/www/admkrsk/common/config/ADMKRSK-TEST-SERVICE-SITE.pfx";
+
+        if($attachment && file_exists($attachment)){
+            $toReplace = '<xop:Include href="cid:5aeaa450-17f0-4484-b845-a8480c363444" xmlns:xop="http://www.w3.org/2004/08/xop/include" />';
+            $fp = fopen($attachment, "rb");
+            $binary = fread($fp, filesize($attachment));
+            $attachment64 = base64_encode($binary);
+            $attachment64 = chunk_split($attachment64, 76, "\r\n"); 
+            $digest = base64_encode(pack('H*', hash('sha1',$binary)));  // считаем дайджест архива: хэш sha1 -> ASCII -> base64
+            //$digest = base64_encode(sha1($binary));
+            //echo ("Подписываем файл: $sourcePath<br>");
+            //echo ("Дайджест: $digest<br>");
+
+            $sourceText = file_get_contents($sourcePath); 
+            //$sourceText = str_replace($toReplace, $attachment64, $sourceText); // заменям ссылку файлоы (возможно, не надо)
+            $sourceText = str_replace('ATTDIGESTHERE', $digest, $sourceText); // записываем дайджест ФАЙЛа (дайдже xml запишется при подписи)
+
+            $tempPath = str_replace('.xml', '_temp.xml', $sourcePath); // формирум файл, который будем подписывать
+            file_put_contents($tempPath,$sourceText);
+            $sourcePath = $tempPath;
+        }
+
+        $xmlSigner = new XmlSigner();
+        $xmlSigner->loadPfxFile($certName, 'CdtDblGfh');
+        $xmlSigner->signXmlFile( $sourcePath, $resultPath, DigestAlgorithmType::SHA1); // подписали
+
+        //echo ("Подписываем файл: $sourcePath<br>");
+        //echo ("Результирующий файл: $resultPath<br>");
+
+        if($attachment && file_exists($attachment)){
+            $resultXML = file_get_contents($resultPath);
+            //$resultXML = str_replace($attachment64, $toReplace, $resultXML);
+            file_put_contents($resultPath,$resultXML); // заменили файл на ссылку назад (возмжно, лишнее)
+        }
+    }
+
+    public function xopCreate($archivePath)
+    {
+        $source = '/var/www/admkrsk/common/config/template_attachment_2.xml';
+        $xmlPath = '/var/www/admkrsk/frontend/runtime/tmp/signed'.time().'.xml';
+        $output = '/var/www/admkrsk/frontend/runtime/tmp/tosend'.time().'.txt';
+        //$attachment = Yii::getAlias('@app').'/assets/6995_req_7a06c1c5-0218-4672-a6eb-7ef46529803e.zip';
+        $attachment = $archivePath;
+
+        $this->signServiceXML($source, $xmlPath, $attachment);
+
+        if(!file_exists($xmlPath))
+            return false;
+
+        $mtomHeader = <<<MTOMHEAD
+MIME-Version: 1.0
+Content-Type: multipart/related; start="<rootpart*c73c9ce8-6e02-40ce-9f68-064e18843428>"; start-info="text/xml"; type="application/xop+xml"; boundary="MIME_boundary";
+
+
+--MIME_boundary
+Content-Type: application/xop+xml;charset=utf-8;type="text/xml"
+Content-Id: <rootpart*c73c9ce8-6e02-40ce-9f68-064e18843428>
+Content-Transfer-Encoding: binary
+
+
+MTOMHEAD;
+
+        $mtomArchivehead = <<<MTMOARCH
+
+--MIME_boundary
+Content-Type: application/zip
+Content-Id: 5aeaa450-17f0-4484-b845-a8480c363444
+Content-Transfer-Encoding: base64
+
+MTMOARCH;
+
+        $mtomClose = "--MIME_boundary--";
+
+        if(file_exists($attachment))
+        {
+            $fp = fopen($attachment, "rb");
+            $binary = fread($fp, filesize($attachment));
+            $attachment64 = base64_encode($binary);  
+            $attachment64 = chunk_split($attachment64, 76, "\r\n");          
+        }
+
+        $result = $mtomHeader;
+        $result .= file_get_contents($xmlPath);
+        $result .= $mtomArchivehead;
+        $result .= "\r\n" . $attachment64 . "\r\n";
+        $result .= "\r\n" . $mtomClose;
+
+        return file_put_contents($output, $result)?$output:false;
+    }
+
     protected function fillRequestTemplate(array $params)
     {
         $placeholders = [
           '{service name}', // reestr_number
-
         ];
 
         $appDataItems = [];
--- frontend/controllers/ServiceController.php
+++ frontend/controllers/ServiceController.php
@@ -336,11 +336,16 @@ class ServiceController extends Controller
                         $export_path = $record->collection->form->makeDoc($record, ['number_internal' => (string)$appeal->number_internal, 'date' => date("d.m.Y", $appeal->date)]);
 
                         $wf = new Workflow;
-                        $wf->generateArchive($idents['guid'], $attachments, $export_path);
-                        die();
+                        $archivePath = $wf->generateArchive($idents['guid'], $attachments, $export_path);
                         // ... тут XML
+                        if($archivePath)
+                            $wf->xopCreate($archivePath);
+
+                        die();
                         
-                        //$opres = $wf->sendServiceMessage($appeal);
+                        $opres = $wf->sendServiceMessage($appeal);
+
+
                         $integration = new Integration;
                         $integration->system = Integration::SYSTEM_SED;
                         $integration->direction = Integration::DIRECTION_OUTPUT;
