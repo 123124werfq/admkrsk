ea89a2f0753afd262aa966ee32f6037701c8c732
Lobzenko <ms@lobzenko.ru>
2020-08-27 13:47:25 +0700

    up

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -30,7 +30,7 @@ class AppAsset extends AssetBundle
         'js/moment-with-locales.min.js',
         'js/jquery.daterangepicker.min.js',
         'js/main.js',
-        'js/site.js?v=6',
+        'js/site.js?v=7',
     ];
     public $depends = [
         'yii\web\YiiAsset',
