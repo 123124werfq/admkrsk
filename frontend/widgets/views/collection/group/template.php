07dad399f8084cff419bc61603e72c373b41dae6
Lobzenko <ms@lobzenko.ru>
2020-09-08 16:37:46 +0700

    up

--- frontend/widgets/views/collection/group/template.php
+++ frontend/widgets/views/collection/group/template.php
@@ -1,6 +1,6 @@
 <?php use yii\widgets\Pjax;?>
 
-<?=$this->render('_search',['unique_hash'=>$unique_hash,'search_columns'=>$search_columns,'pagesize'=>$pagesize])?>
+<?=$this->render('../_search',['unique_hash'=>$unique_hash,'search_columns'=>$search_columns,'pagesize'=>$pagesize])?>
 
 <?php Pjax::begin([
 	'id' => $unique_hash,
