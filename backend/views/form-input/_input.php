ed587e25cff9925637b84aed6d950710933bbbef
Lobzenko <ms@lobzenko.ru>
2021-04-08 15:22:05 +0700

    up

--- backend/views/form-input/_input.php
+++ backend/views/form-input/_input.php
@@ -58,7 +58,7 @@
 	    else if ($visibleInput->type==CollectionColumn::TYPE_COLLECTION)
 	    {
 	    	echo $form->field($model, "[visibleInputs][$rowKey]values",['template'=>"{input}"])->widget(Select2::class, [
-                'data' => $model->values,
+                'data' => $visibleInput->getSelected($model->values),
                 'pluginOptions' => [
                     'multiple' => true,
                     'allowClear' => true,
