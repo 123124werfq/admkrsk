8f88a9dfd23d5136fab01b33562ad3e0aaced58e
Lobzenko <ms@lobzenko.ru>
2021-05-11 14:16:38 +0700

    up

--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -57,6 +57,11 @@ function addInput(block)
 
 $(document).ready(function() {
 
+    $(".content.searchable table").each(function(){
+        if (!$(this).parent().hasClass('table-responsive'))
+            $(this).parent().addClass('table-responsive');
+    });
+
     $(".ajax-form").on('beforeSubmit', function (event) {
         event.preventDefault();
 
@@ -293,7 +298,7 @@ $(document).ready(function() {
             isRTL: false,
             showMonthAfterYear: false,
             yearSuffix: ''};
-        
+
         $.datepicker.setDefaults($.datepicker.regional['ru']);
 
         $("#datepicker").datepicker({
