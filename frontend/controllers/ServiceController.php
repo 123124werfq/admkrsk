ceb228d895432c38b4d2014bb5dfe05c8825adf8
Konstantin Bogdanov <kosyag@gmail.com>
2022-03-01 21:25:47 +0700

    upd

--- frontend/controllers/ServiceController.php
+++ frontend/controllers/ServiceController.php
@@ -375,7 +375,7 @@ class ServiceController extends Controller
                         $wf = new Workflow;
                         $archivePath = $wf->generateArchive($idents['guid'], $attachments, $export_path);
 
-                        var_dump($appeal->target->id_form); 
+                        var_dump($appeal->target->form->fullname); 
                         //var_dump($appeal->service->subject); // описание (шаблон)
                         //var_dump($insertedData); // все данные из формы
                         die();
