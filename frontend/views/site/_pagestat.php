8d05c47c9cfe22f95f873496308aa9bdf06a64e1
Konstantin Bogdanov <kosyag@gmail.com>
2022-03-03 15:41:56 +0700

    скрыли просмотров за год

--- frontend/views/site/_pagestat.php
+++ frontend/views/site/_pagestat.php
@@ -2,7 +2,7 @@
     <div class="col-2-third">
         <p class="text-help">
             <?=Yii::t('site', 'Дата публикации (изменения)')?>: <span class="publish-date"><?=date('d.m.Y',$data->created_at)?></span> (<span class="update-date"><?=date('d.m.Y',$data->updated_at)?></span>)<br>
-            <?=Yii::t('site', 'Просмотров за год (всего)')?>: <?=$data->viewsYear?> (<?=$data->views?>)
+            <!--<?=Yii::t('site', 'Просмотров за год (всего)')?>: <?=$data->viewsYear?> (<?=$data->views?>)-->
         </p>
         <div class="subscribe">
             <div class="subscribe_left">
