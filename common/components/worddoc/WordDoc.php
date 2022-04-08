40ac27a16c24dbe1f91f0729c0d456bd2eff3203
lobzenko <ms@lobzenko.ru>
2022-04-08 13:53:01 +0700

    up

--- common/components/worddoc/WordDoc.php
+++ common/components/worddoc/WordDoc.php
@@ -77,7 +77,6 @@ class WordDoc
                             $i++;
                         }
                     }
-                    /
                     catch(\Exception $e) {
                      
                     }
