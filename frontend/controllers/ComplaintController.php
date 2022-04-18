2188ec00722a701e0373f3795d1af7ca8730716c
Konstantin Bogdanov <kosyag@gmail.com>
2022-04-18 12:06:05 +0700

    upd

--- frontend/controllers/ComplaintController.php
+++ frontend/controllers/ComplaintController.php
@@ -215,10 +215,10 @@ class ComplaintController extends \yii\web\Controller
 
                         //var_dump($appeal->target->form->fullname); 
                         //var_dump($appeal->service->subject); // описание (шаблон)
-                        var_dump($insertedData); // все данные из формы
+                        //var_dump($insertedData); // все данные из формы
                         //$esiaUser = EsiaUser::find()->where(['id_user' => Yii::$app->user->id])->one();
                         //var_dump($esiaUser->snils);
-                        die();
+                        //die();
 
                         // ... тут XML
                         if($archivePath)
